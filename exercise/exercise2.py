a6795a5c12cf1e20cf475461f08455250ad61ec1
kyiithelord <thetzinkyaw9727@gmail.com>
2024-10-03 09:24:20 +0630

    operator

--- exercise/exercise2.py
+++ exercise/exercise2.py
@@ -2,6 +2,6 @@ for i in range(0,10):
     currentNumber = i
     previousNumber = i -1
     if i == 0:
-        previousNumber = 0
+        previousNumber = -1
     totalNumber = previousNumber + currentNumber
     print('the current number is :',i,"previous number is :",previousNumber,'the total number is :',totalNumber)
